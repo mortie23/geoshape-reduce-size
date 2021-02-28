0c028e48b7e4262be7bea0bd3e252d66715b0380
Christopher Mortimer <christopher@mortimer.xyz>
2021-03-01 09:21:31 +1100

    reduce to repo

--- convert.py
+++ convert.py
@@ -5,6 +5,6 @@ import geopandas
 inputfile='/mnt/c/data/geodata/RA_2016_AUST/RA_2016_AUST.shp'
 myshpfile = geopandas.read_file(inputfile)
 print(inputfile+': file read')
-outputfile='/mnt/c/data/geodata/RA_2016_AUST/RA_2016_AUST.json'
+outputfile='/mnt/c/data/geodata/RA_2016_AUST/RA_2016_AUST.geojson'
 myshpfile.to_file(outputfile, driver='GeoJSON') 
 print(outputfile+': file written')
